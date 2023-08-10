05f079f989f906ce28ae6b8f7e09f688ee8f837f
shaberry <jaden.png02@gmail.com>
2023-08-10 12:35:35 -0700

    egg

--- Sales-Dashboard/src/App.jsx
+++ Sales-Dashboard/src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
-import PrimaryDashboard from './components/PrimaryDashboard'
-import SideBar from './components/SideBar'
-import TransactionBar from './components/TransactionBar'
+import PrimaryDashboard from './components/PrimaryDashboard';
+import SideBar from './components/SideBar';
+import TransactionBar from './components/TransactionBar';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+
 
 
 function App() {
 
   return (
     <>
+    <Grid container spacing={3}>
     <PrimaryDashboard/>
     <TransactionBar/>
-      <SideBar/>
-    </>
+    <SideBar/>
+   </Grid> 
+   </>
   )
 }
 
